db30c53eb3488eb3752fc4ccf34656b9cef076e0
chiri <y_takeuchi_137@yahoo.co.jp>
2014-02-08 11:52:31 +0900

    Update my_fgets.c

--- my_fgets.c
+++ my_fgets.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 
-void my_fgets(char s[],int n,FILE *fp){
-int i=0;
-while(s[i]=getchar() != EOF){
-if(s[i]=='\n')break;
-i++;
-}
+void my_fgets(char s[],int n,FILE *fp)
+{
+  int i=0;
+  while(s[i]=getchar() != EOF)
+  {
+    if(s[i]=='\n')break;
+    i++;
+  }
 }
 
-int main(){
-char s[100];
-my_fgets(s,100.stdin);
-printf("%s",s);
-return 0;
+int main()
+{
+  char s[100];
+  my_fgets(s,100.stdin);
+  printf("%s",s);
+  return 0;
 }
 
